f0217b598284f1878053e3218e82a87338efa055
Mitchell Flood <mitchflood1@gmail.com>
2022-02-21 15:35:33 -0700

    using document.queryselector instead of getelement byid

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
 /* eslint-disable prettier/prettier */
 
 
-const key = document.getElementById('key');
+// const key = document.getElementById('key');
+const key = document.querySelector('key');
 document.addEventListener('keydown', logKey);
+
 
 function logKey(e) {
     console.log(e)
