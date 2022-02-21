368d5de9783a2dbfd8e5621e62d533b96be1e365
Mitchell Flood <mitchflood1@gmail.com>
2022-02-21 15:39:35 -0700

    starting audio function by linking to keydown (e)

--- script.js
+++ script.js
@@ -3,12 +3,18 @@
 /* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
 
-
-// const key = document.getElementById('key');
 const key = document.querySelector('key');
 document.addEventListener('keydown', logKey);
 
+const audio = document.querySelector('audio');
+document.addEventListener('keydown', playSound);
+
+
 
 function logKey(e) {
+    console.log(e)
+};
+
+function playSound(e) {
     console.log(e)
 };
